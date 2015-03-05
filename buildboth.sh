24066bdf9ea6f85bb0201f9c772b0a9c2737b00d
Shawn Cicoria <shawn@cicoria.com>
2015-03-05 17:54:08 -0500

    new

--- buildboth.sh
+++ buildboth.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
-#cd mongo
-#docker build -t scicoria/mongo:0.1 .
 cd seed
 docker build -t scicoria/seedmongo:0.1 .
 cd ../orderService
--- runboth.sh
+++ runboth.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
-#docker run -d -p 27017:27017 --name mongodb01 scicoria/mongo:0.1
 docker run -d -p 27017:27017 --name mongodb -v /data/db:/data/db mongo:2.6.7 --smallfiles
-docker run -d scicoria/seedmongo:0.1
-docker run -d -p 8080:8080 scicoria/orderservice:0.1
+docker run -d -p 8080:8080 --link mongodb:mongo scicoria/orderservice:0.1
