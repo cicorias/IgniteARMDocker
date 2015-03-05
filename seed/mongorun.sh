45fdf85439f4e10b44fe79f85b2d4b2a1e249b64
Shawn Cicoria <shawn@cicoria.com>
2015-03-05 15:53:18 -0500

    more changes

---
+++ seed/mongorun.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+/usr/bin/mongoimport -d ordering -c catalog < /tmp/mongodb.catalog.json
+/usr/bin/mongoimport -d ordering -c dealers < /tmp/mongodb.dealers.json
+/usr/bin/mongoimport -d ordering -c quotes < /tmp/mongodb.quotes.json
+
+
+/usr/bin/mongod
