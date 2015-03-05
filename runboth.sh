8927a8a883124a9ac94c651c9db49f5dc2cf9763
Shawn Cicoria <shawn@cicoria.com>
2015-03-05 18:54:54 -0500

    correct both run

--- runboth.sh
+++ runboth.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-docker run -d -p 27017:27017 --name mongodb -v /data/db:/data/db mongo:2.6.7 --smallfiles
+docker run -p 27017:27017 --name mongodb -v /data/db:/data/db scicoria/seedmongo:0.1
 docker run -d -p 8080:8080 --link mongodb:mongo scicoria/orderservice:0.1
