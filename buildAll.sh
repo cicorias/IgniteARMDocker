7017d0351f8588ab860a94582c55ca5a2f465027
Ubuntu <cicorias@docker2.docker2-v6vk67py.d2.internal.cloudapp.net>
2015-03-11 21:38:30 +0000

    updates

--- buildAll.sh
+++ buildAll.sh
@@ -3,4 +3,7 @@ cd mongoseed
 docker build -t scicoria/mongoseed:0.1 .
 cd ../orderService
 docker build -t scicoria/orderservice:0.1 .
-cd ../
+cd ../staticsite
+docker build -t scicoria/staticsite:0.1 .
+cd ..
+
